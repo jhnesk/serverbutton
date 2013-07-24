19c6f1a1b3b6d9cd72451fdd74537dd0c02ef3b2
Johan Ask <johan.ask@netset.se>
2013-07-24 14:08:23 +0200

    Removed deprecated default preferences

--- firefox/defaults/preferences/commands.js
+++
@@ -1,4 +0,0 @@
-pref("extensions.serverbutton.command.ssh", "/usr/bin/urxvt -e ssh $USER@$HOST");
-pref("extensions.serverbutton.command.ftp", "/usr/bin/gftp ftp://$USER:$PASSWORD@$HOST");
-pref("extensions.serverbutton.command.rdesktop", "/usr/bin/rdesktop -u $USER -p $PASSWORD -g 1280x960 $HOST");
-pref("extensions.serverbutton.command.vmware", "/usr/bin/vmware-view --serverURL=$HOST --userName=$USER --password=$PASSWORD -q");
